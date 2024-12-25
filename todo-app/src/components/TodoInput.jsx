860969e86c0b612f38f168514697c8018c793e05
Warsame Egal <warsameegal0@gmail.com>
2024-12-24 22:43:08 -0500

    Setup React + Vite Project with Tailwind CSS
    - Configured Tailwind CSS
    - Created initial components: Header, Tabs, ToDoCard, ToDoInput, ToDoList

---
+++ todo-app/src/components/TodoInput.jsx
@@ -0,0 +1,5 @@
+export function ToDoInput() {
+    return (
+        <dive></dive>
+    )
+}
---
+++ todo-app/src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import { TodoCard } from "./ToDoCard";
+
+export function ToDoList() {
+    return (
+        <div>
+            <TodoCard />
+
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+        </div>
+    )
+}
